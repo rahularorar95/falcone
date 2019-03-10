81ce0c2651fdbba354ec0a1d861cc0fe69023edc
Rahul Arora <rahularorar95@gmail.com>
2019-03-10 23:41:21 +0530

    Anchor tag issue

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ export default (props) => {
                 <h1 className="brand">Finding Falcone !</h1>
                 <div className="ui inverted menu right-menu">
                 <a className='item' onClick ={()=>props.resetState()} >Reset</a>
-                <a className='item' href="https://www.geektrust.in/" target="_blank">GeekTrust</a>
+                <a className='item' href="https://www.geektrust.in/" target="_blank" rel="noopener noreferrer">GeekTrust</a>
                 </div>
             </div>
         </div>
